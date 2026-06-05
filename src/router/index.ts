bdbb2eccd076b5f1869e170fecb1dfb0e17d15d7
xiao-e-yun <me@xiaoeyun.me>
2026-06-05 14:33:45 +0800

    chore: remove About page and route

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,11 +10,6 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/crop',
       name: 'crop',
       component: () => import('../views/CropView.vue'),
